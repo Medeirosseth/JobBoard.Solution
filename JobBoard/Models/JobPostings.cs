73ab752d605a5f3f0969f6fd2ef60797c9b7c2a4
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-11 15:02:14 -0700

    add mvc file tree/gitignore
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>
    Co-authored-by: Ahmed Ghouzlane <ahmedghouzlane@gmail.com>

---
+++ JobBoard/Models/JobPostings.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace JobBoard.Models
+{
+    public class JobPostings
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string ContactInfo { get; set; }
+
+
+        private static List<JobPostings> _jobOpenings = new List<JobPostings> {};
+
+        public JobPostings (string title, string description, string contactInfo )
+
+        {
+            Title = title;
+            Description = description;
+            ContactInfo = contactInfo;
+
+            _jobOpenings.Add(this);
+        }
+
+        public static List<JobPostings> GetAll()
+        {
+            return _jobOpenings;
+        }
+    }
+}
